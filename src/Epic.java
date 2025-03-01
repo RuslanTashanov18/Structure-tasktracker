fe13c276693e9bc3ce45ccab3c6135a19a513c9e
RuslanTashanov18 <tashanov.2019@gmail.com>
2025-03-01 13:29:25 +0500

    1 версия Структуры task tracker

---
+++ src/Epic.java
@@ -0,0 +1,10 @@
+public class Epic {
+    private int id;
+    private String epicName;
+    private String status;
+    public Epic(String epicName, int id, String status){
+        this.epicName = epicName;
+        this.id = id;
+        this.status = status;
+    }
+}
---
+++ src/Subtask.java
@@ -0,0 +1,10 @@
+public class Subtask {
+    private int id;
+    private String subtaskName;
+    private String status;
+    public Subtask(String subtaskName, int id, String status){
+        this.subtaskName = subtaskName;
+        this.id = id;
+        this.status = status;
+    }
+}
